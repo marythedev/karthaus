d75237e986d3c3888c0a6dbd3056f83467451b05
Maria <79389256+marythedev@users.noreply.github.com>
2024-02-13 21:47:03 -0500

    updated frontend to test how it interacts with backend

--- Code/frontend/src/App.js
+++ Code/frontend/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
 
+// when GET button clicked make fetch to http://localhost:8080/api
+
+
 function App() {
+
+  const fetchGet = () => {
+    console.log("Fetching Get from backend...");
+    fetch("http://localhost:8080/api", {
+      method: "GET"
+    }).then(res => res.text())
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+  }
+
+  const fetchPost = () => {
+    console.log("Fetching Post from backend");
+    fetch("http://localhost:8080/api", {
+      method: "POST"
+    }).then(res => res.text())
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Welcome to our online shop :)</h1>
+      <button onClick={fetchGet}>Backend Test: GET</button>
+      <button onClick={fetchPost}>Backend Test: POST</button>
     </div>
   );
 }
