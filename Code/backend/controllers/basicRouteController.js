3ad49c2f4a141428d5e25315794c4befb6c3ec41
Stephen V <svizl1@myseneca.ca>
2024-03-15 21:20:28 -0400

    Added Code Comments

--- Code/backend/controllers/basicRouteController.js
+++ Code/backend/controllers/basicRouteController.js
@@ -1,12 +1,14 @@
+// Return Server Status
 exports.default = (req, res) => {
   res.send("Server is running");
 };
 
+// Test Route for GET
 exports.get = (req, res) => {
   res.json({ message: "GET request received" });
 };
 
-// Handle POST request
+// Test Route for POST
 exports.post = (req, res) => {
   res.json({ message: "POST request received" });
 };
