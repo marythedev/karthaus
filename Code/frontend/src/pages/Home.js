86875f9936ded4f256a1985f055abcf39aadb053
Maria <79389256+marythedev@users.noreply.github.com>
2024-04-03 10:40:29 -0400

    added img carousel

--- Code/frontend/src/pages/Home.js
+++ Code/frontend/src/pages/Home.js
@@ -1,8 +1,34 @@
 import React from 'react';
 import './Home.css';
+import Carousel from 'react-bootstrap/Carousel';
+
+function UncontrolledCarousel() {
+  return (
+    <Carousel>
+      <Carousel.Item>
+        <img src={window.location.origin + '/images/placeholder-img.jpg'} alt="Placeholder img" class="img-fluid" />
+        <Carousel.Caption>
+          <h1>Tech Setup</h1>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src={window.location.origin + '/images/placeholder-img.jpg'} alt="Placeholder img" class="img-fluid" />
+        <Carousel.Caption>
+          <h1>Tech Setup</h1>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src={window.location.origin + '/images/placeholder-img.jpg'} alt="Placeholder img" class="img-fluid" />
+        <Carousel.Caption>
+          <h1>Tech Setup</h1>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
 
 const Home = () => {
-  return ( 
+  return (
     <div className="home-body">
       <ul id="home-category-nav">
         <li><a href="#">Tech</a></li>
@@ -15,8 +41,14 @@ const Home = () => {
         <li><a href="#">Shoes</a></li>
         <li><a href="#">Other</a></li>
       </ul>
-      
-      <h1>Welcome to our online shop :)</h1>
+
+      <div id="carousel">
+          <UncontrolledCarousel />
+      </div>
+
+      <br></br>
+      <br></br>
+      <br></br>
     </div >
   );
 }
