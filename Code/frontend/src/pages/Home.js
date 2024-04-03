c06bd83239c787ff01e2f53acd861cd2f59021e9
Maria <79389256+marythedev@users.noreply.github.com>
2024-04-03 09:57:18 -0400

    added home page category subnav (large screens only)

--- Code/frontend/src/pages/Home.js
+++ Code/frontend/src/pages/Home.js
@@ -5,15 +5,15 @@ const Home = () => {
   return ( 
     <div className="home-body">
       <ul id="home-category-nav">
-        <li>Tech</li>
-        <li>Outdoor Gear</li>
-        <li>Home Appliences</li>
-        <li>Women's</li>
-        <li>Men's</li>
-        <li>Kids'</li>
-        <li>Accessories</li>
-        <li>Shoes</li>
-        <li>Other</li>
+        <li><a href="#">Tech</a></li>
+        <li><a href="#">Outdoor&nbsp;Gear</a></li>
+        <li><a href="#">Home&nbsp;Appliences</a></li>
+        <li><a href="#">Women's</a></li>
+        <li><a href="#">Men's</a></li>
+        <li><a href="#">Kids'</a></li>
+        <li><a href="#">Accessories</a></li>
+        <li><a href="#">Shoes</a></li>
+        <li><a href="#">Other</a></li>
       </ul>
       
       <h1>Welcome to our online shop :)</h1>
